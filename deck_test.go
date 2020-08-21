2e414937e6423fde3f335a99df80ba8d4b19855b
Giri Jeedigunta <giri.jeedigunta@gmail.com>
2020-08-21 18:57:07 +0530

    Added unit tests

---
+++ deck_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck();
+
+	if len(d) != 52 {
+		t.Errorf("Expected deck length of 52 but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Clubs" {
+		t.Errorf("Expected Clubs of Ace but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Spades" {
+		t.Errorf("Expected King of Spades but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_deckTesting")
+
+	deck := newDeck()
+	deck.saveToFile("_deckTesting")
+
+	loadedDeck := newDeckFromFile("_deckTesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected deck length of 52 but got %v", len(loadedDeck))
+	}
+	
+	os.Remove("_deckTesting")
+}
